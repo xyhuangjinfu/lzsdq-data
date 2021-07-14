8aa7686323ff53c45da764a2fa6359d1bab7f480
xyhuangjinfu <630569027@qq.com>
2021-07-14 08:59:14 +0800

    sitemap

--- sitemap.py
+++ sitemap.py
@@ -1,7 +1,30 @@
 import db
 
 
-if __name__ == '__main__':
+def create_sitemap():
     article_list = db.get_all_article()
-    for a in article_list:
-        print(f"{a['id']}   {a['title']}   {a['create_time']}")
+
+    fp = open("./assets/sitemap.xml", "w+", encoding="utf-8")
+    fp.seek(0)
+    fp.truncate()
+
+    fp.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    fp.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+
+    for article in article_list:
+        fp.write('  <url>\n')
+        fp.write(f'    <loc>https://www.lzsdq.cn/article/{article["id"]}</loc>\n')
+        fp.write(f'    <lastmod>{_get_date(article["create_time"])}</lastmod>\n')
+        fp.write('  </url>\n')
+
+    fp.write('</urlset>\n')
+    fp.close()
+
+
+def _get_date(datetime_obj):
+    date_str = datetime_obj.strftime("%Y-%m-%d")
+    return date_str
+
+
+if __name__ == '__main__':
+    create_sitemap()
